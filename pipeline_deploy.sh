395a0db0e03c75e6feec7b07542bc569a985cf39
Brian Kirkland <bkirkland@bespin.local>
2019-07-30 18:54:45 -0400

    kirklab changes

--- pipeline_deploy.sh
+++ pipeline_deploy.sh
@@ -11,7 +11,7 @@ BUILD_ARTIFACT=${APP}
 RELEASE_REPOSITORY=${APP}
 RELEASE_OWNER=bkirkware
 
-fly sp -t ci -c pipeline-deploy.yml -p ${PIPELINE_NAME} -l creds.yml \
+fly sp -t kirklab -c pipeline-deploy.yml -p ${PIPELINE_NAME} -l creds.yml \
   -v build_artifact=${BUILD_ARTIFACT} \
   -v resource_concourse_branch=master \
   -v resource_concourse_uri=https://github.com/bkirkware/todos-concourse.git \
